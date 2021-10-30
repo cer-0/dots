79ea2d769618f8125b06fe03bc8fad63cfd8fced
cer-0 <cer0@riseup.net>
2021-10-30 16:49:22 -0500

    Change font size

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -23,7 +23,7 @@
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 (setq doom-font (font-spec :family "Terminus" :size 14)
       ; doom-variable-pitch-font (font-spec :family "Fira Sans" :size 12)
-      doom-big-font (font-spec :family "Terminus" :size 20))
+      doom-big-font (font-spec :family "Terminus" :size 22))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
