33fd526712bd8ca70bd7c567be52a8b1670936f0
cer-0 <cer0@riseup.net>
2022-07-24 23:32:47 -0500

    Use material-gruvbox, change font, add tools
    - Use material-gruvbox as the main theme, taken from
    - Change to JetBrains Mono font
    - Add ansible and docker as tools to init.el
    - Delete a few lines of comments D:

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -1,34 +1,28 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name ""
       user-mail-address "")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
+;; Doom exposes five (optional) variables for controlling fonts in Doom.
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
-(setq doom-font (font-spec :family "IBM Plex Mono" :size 13)
+(setq doom-font (font-spec :family "JetBrains Mono" :size 13)
       doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 12)
-      doom-big-font (font-spec :family "IBM Plex Mono" :size 22))
+      doom-big-font (font-spec :family "JetBrains Mono" :size 22))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-gruvbox) ; moonlight nord oceanic-next palenight city-lights
+
+(setq doom-gruvbox-material-background  "medium"  ; or hard (defaults to soft)
+     doom-gruvbox-material-palette     "material") ; or original (defaults to material)
+(setq doom-gruvbox-material-light-background  "medium" ; or hard (defaults to soft)
+      doom-gruvbox-material-light-palette     "material") ; or original (defaults to material)
+(setq doom-theme 'doom-gruvbox-material)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -37,7 +31,6 @@
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type 'relative)
-
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
