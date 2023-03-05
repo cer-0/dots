730f64744eafa34b881f71c5949af95358c524e7
cer-0 <cer0@riseup.net>
2023-03-05 12:10:27 -0600

    Update bootstrap script

--- nvim/.config/nvim/lua/bootstrap.lua
+++ nvim/.config/nvim/lua/bootstrap.lua
@@ -1,47 +1,51 @@
-local PKGS = {
-	"savq/paq-nvim";
-	"hoob3rt/lualine.nvim";
-	"nvim-telescope/telescope.nvim";
-	"nvim-lua/popup.nvim";
-	"nvim-lua/plenary.nvim";
-	"kyazdani42/nvim-tree.lua";
-	"kyazdani42/nvim-web-devicons";
-	"projekt0n/circles.nvim";
-	"nvim-treesitter/nvim-treesitter";
-	"ahmedkhalf/project.nvim";
-	"TimUntersberger/neogit";
-	"terrortylor/nvim-comment";
-	"windwp/nvim-autopairs";
-	"junegunn/rainbow_parentheses.vim";
-	"Pocco81/TrueZen.nvim";
-	"sainnhe/gruvbox-material";
-	"arcticicestudio/nord-vim";
-	"navarasu/onedark.nvim";
-}
-
 local function clone_paq()
-	local path = vim.fn.stdpath('data') .. '/site/pack/paqs/start/paq-nvim'
-	if vim.fn.empty(vim.fn.glob(path)) > 0 then
-		vim.fn.system {
-			'git',
-			'clone',
-			'--depth=1',
-			'https://github.com/savq/paq-nvim.git',
-			path
-		}
-	end
+  local path = vim.fn.stdpath("data") .. "/site/pack/paqs/start/paq-nvim"
+  local is_installed = vim.fn.empty(vim.fn.glob(path)) == 0
+  if not is_installed then
+    vim.fn.system { "git", "clone", "--depth=1", "https://github.com/savq/paq-nvim.git", path }
+    return true
+  end
 end
 
-local function bootstrap_paq()
-	clone_paq()
-	-- Load Paq
-	vim.cmd('packadd paq-nvim')
-	local paq = require('paq')
-	-- Exit nvim after installing plugins
-	vim.cmd('autocmd User PaqDoneInstall quit')
-	-- Read and install packages
-	paq(PKGS)
-	paq.install()
+local function bootstrap_paq(packages)
+  local first_install = clone_paq()
+  vim.cmd.packadd("paq-nvim")
+  local paq = require("paq")
+  if first_install then
+    vim.notify("Installing plugins... If prompted, hit Enter to continue.")
+  end
+
+  -- Read and install packages
+  paq(packages)
+  paq.install()
 end
 
-return { bootstrap_paq = bootstrap_paq }
+local function headless_paq()
+  -- Set to exit nvim after installing plugins
+  vim.cmd("autocmd User PaqDoneInstall quit")
+  bootstrap_paq {
+    "savq/paq-nvim",
+    -- List your packages
+    "hoob3rt/lualine.nvim",
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/popup.nvim",
+    "nvim-lua/plenary.nvim",
+    "kyazdani42/nvim-tree.lua",
+    "kyazdani42/nvim-web-devicons",
+    "projekt0n/circles.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "ahmedkhalf/project.nvim",
+    "TimUntersberger/neogit",
+    "terrortylor/nvim-comment",
+    "windwp/nvim-autopairs",
+    "junegunn/rainbow_parentheses.vim",
+    "Pocco81/TrueZen.nvim",
+    "sainnhe/gruvbox-material",
+    "arcticicestudio/nord-vim",
+    "navarasu/onedark.nvim",
+  }
+end
+
+return {
+  headless_paq = headless_paq,
+}
